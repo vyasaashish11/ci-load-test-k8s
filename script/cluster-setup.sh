06bf40fb80e821de411d91d525367221fb831dd7
vyasaashish11 <88895102+vyasaashish11@users.noreply.github.com>
2026-01-12 13:44:17 +0530

    Update cluster-setup.sh

--- script/cluster-setup.sh
+++ script/cluster-setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -e
 
+CLUSTER_NAME="ci-cluster"
+
 echo "Installing KinD..."
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
-echo "Creating Kubernetes cluster..."
-kind create cluster --config /home/system/CI-Load-Test/k8s/kind.config.yaml --name ci-cluster
+echo "Deleting existing cluster (if any)..."
+kind delete cluster --name $CLUSTER_NAME || true
+
+echo "Creating Kubernetes cluster with 2 workers..."
+kind create cluster --config k8s/kind-config.yaml --name $CLUSTER_NAME
 
 echo "Cluster created"
 
 kubectl cluster-info
 kubectl get nodes
-
